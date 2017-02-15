132c770c1e06d6c6da654143f847d5c6db3f76e8
qylibohao <375795423@qq.com>
2017-02-15 11:40:21 +0800

    first add

---
+++ BHQiNiuUploadHelper/BHQiNiuUploadHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BHQiNiuUploadHelper
+//
+//  Created by libohao on 17/2/15.
+//  Copyright © 2017年 libohao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
